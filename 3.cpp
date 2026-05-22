412332087fc1bc017b9bca6e22c74e434ce1d3d5
ibrahimmustafacv <memo.ibrahim.2003@gmail.com>
2026-05-22 12:40:10 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,5 @@
+const char* ssid = "Justicar";          // اسم الشبكة المزيفة
+const char* password = "48522844";      // كلمة مرور الشبكة
+IPAddress local_ip(192, 168, 1, 4);     // عنوان IP للوحة التحكم
+const char* admin_user = "admin";       // اسم مستخدم لوحة التحكم
+const char* admin_pass = "48522844";    // كلمة مرور لوحة التحكم
